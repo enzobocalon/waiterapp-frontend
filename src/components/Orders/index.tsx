d76d6e03630c494fecdc9b491ae6c3178073bdab
Enzo Bocalon <enzo.bocalon@hotmail.com>
2022-12-05 22:19:46 -0300

    feat: order data

--- src/components/Orders/index.tsx
+++ src/components/Orders/index.tsx
@@ -1,13 +1,26 @@
 import { OrdersBoard } from '../OrdersBoard';
 import { Container } from './style';
-import { orders } from '../../mocks/orders';
+import { useEffect, useState } from 'react';
+import { Order } from '../../types/Order';
+import { api } from '../../utils/api';
 
 export function Orders() {
+  const [orders, setOrders] = useState<Order[]>([]);
+
+  useEffect(() => {
+    api.get('/orders').then(({ data }) => {
+      setOrders(data);
+    });
+  }, []);
+
+  const waiting = orders.filter(order => order.status === 'WAITING');
+  const inProduction = orders.filter(order => order.status === 'IN_PRODUCTION');
+  const done = orders.filter(order => order.status === 'DONE');
   return (
     <Container>
-      <OrdersBoard icon='🕓' title='Fila de espera' orders={orders}/>
-      <OrdersBoard icon='👩‍🍳' title='Em Preparação' orders={[]}/>
-      <OrdersBoard icon='✅' title='Pronto!' orders={[]}/>
+      <OrdersBoard icon='🕓' title='Fila de espera' orders={waiting}/>
+      <OrdersBoard icon='👩‍🍳' title='Em Preparação' orders={inProduction}/>
+      <OrdersBoard icon='✅' title='Pronto!' orders={done}/>
     </Container>
   );
 }
